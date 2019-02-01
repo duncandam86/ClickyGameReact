3afdc95ef8df751c793dcdaa5169c9af2263e0bf
Duncan Dam <duncandam@duncans-mbp.lan>
2019-02-01 16:19:07 -0600

    Updates

---
+++ precache-manifest.b697013b980b338874421ba9dd2c8dd8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "37ced9ef0cb8811affdeda0ed9608a56",
+    "url": "/ClickyGameReact/static/media/Kimchi.37ced9ef.jpg"
+  },
+  {
+    "revision": "6c3386392d51d6aa34f6",
+    "url": "/ClickyGameReact/static/css/main.86049803.chunk.css"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ClickyGameReact/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f92b3e7b15186eeba144",
+    "url": "/ClickyGameReact/static/js/runtime~main.f92b3e7b.js"
+  },
+  {
+    "revision": "377e2bcdff634b698ba8880bb553703c",
+    "url": "/ClickyGameReact/static/media/Alaskathuderfuck.377e2bcd.jpg"
+  },
+  {
+    "revision": "566cd7ee41001de69ee77c3de08dd8b7",
+    "url": "/ClickyGameReact/static/media/AlyssaEdwards.566cd7ee.jpg"
+  },
+  {
+    "revision": "d7028191c90aaac9a5393e115b46ec74",
+    "url": "/ClickyGameReact/static/media/BiancadelRio.d7028191.jpg"
+  },
+  {
+    "revision": "88c048e2342b2ee730195c116f8b821e",
+    "url": "/ClickyGameReact/static/media/Bob.88c048e2.jpg"
+  },
+  {
+    "revision": "ecd857e179b98433d13561fc72bb6472",
+    "url": "/ClickyGameReact/static/media/Ginger.ecd857e1.jpg"
+  },
+  {
+    "revision": "76a2621fe9c8806700f5501bd065761d",
+    "url": "/ClickyGameReact/static/media/Katya.76a2621f.jpg"
+  },
+  {
+    "revision": "6c3386392d51d6aa34f6",
+    "url": "/ClickyGameReact/static/js/main.6c338639.chunk.js"
+  },
+  {
+    "revision": "b8e125a6c502dcaae02bc3d75fd7cffa",
+    "url": "/ClickyGameReact/static/media/LadyBunny.b8e125a6.jpg"
+  },
+  {
+    "revision": "f9dabc06a174a19efe5e2e0ade064242",
+    "url": "/ClickyGameReact/static/media/Latrice.f9dabc06.jpg"
+  },
+  {
+    "revision": "d3dc0430738aeb34a9bd7f824d2e5af9",
+    "url": "/ClickyGameReact/static/media/ManilaLuzon.d3dc0430.jpg"
+  },
+  {
+    "revision": "417a725d5f6c66a1ed589ef75a9cb4d9",
+    "url": "/ClickyGameReact/static/media/MoniqueHeart.417a725d.jpg"
+  },
+  {
+    "revision": "efaec9e5bc675db2bb1b359fa0396471",
+    "url": "/ClickyGameReact/static/media/Raven.efaec9e5.jpg"
+  },
+  {
+    "revision": "82561fc1e473e15001dfa397adbbe466",
+    "url": "/ClickyGameReact/static/media/SheaCoulee.82561fc1.jpeg"
+  },
+  {
+    "revision": "033d8f5e5283721fed8a2f116407e499",
+    "url": "/ClickyGameReact/static/media/TrixieMattel.033d8f5e.jpg"
+  },
+  {
+    "revision": "eb66d8484b10c4e60e6a08923fe90aae",
+    "url": "/ClickyGameReact/static/media/Willam.eb66d848.jpg"
+  },
+  {
+    "revision": "cddd9ec01d696d13b3df81b41772293f",
+    "url": "/ClickyGameReact/static/media/Yuhua.cddd9ec0.jpg"
+  },
+  {
+    "revision": "e9b0293cf9c4c3526ec5085d96b4a66a",
+    "url": "/ClickyGameReact/index.html"
+  }
+];
