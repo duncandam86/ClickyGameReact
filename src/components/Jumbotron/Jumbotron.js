7ca777e3028ded0df155ca25b820b1e1f8bad7a0
Duncan Dam <duncandam@dhcp-10-105-90-77.wireless.northwestern.private>
2019-02-02 10:48:41 -0600

    jumbotron update

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -6,9 +6,9 @@ const Jumbotron = props => (
 	<header className="jumbotron header text-center">
 		<h2>Click on any drag queen to earn a point</h2>
 		<h4>Click on the same queen more than once, you will be doomed.</h4>
-		<h4>Click all 16 queen, and you win.</h4>
-		<p>Score: {props.score}</p>
-		<p>Top Score: {props.topScore}</p>
+		<h4>Click all 16 queens, and you win.</h4>
+		<p className="score">Score: {props.score}</p>
+		<p className="score">Top Score: {props.topScore}</p>
 	</header>
 );
 export default Jumbotron;
